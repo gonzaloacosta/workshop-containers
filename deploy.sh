59b21bafe5d577e3c4712d6c7b5edcc8ebe2175d
Gonzalo Acosta <gonzaloacostapeiro@gmail.com>
2020-06-05 14:24:09 -0300

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #oc new-project workshops
 oc new-app osevg/workshopper:latest --name=workshop-containers \
-	-e CONTENT_URL_PREFIX=https://raw.githubusercontent.com/gonzaloacosta/workshop-containers/master \
-	-e WORKSHOPS_URLS=https://raw.githubusercontent.com/gonzaloacosta/workshop-containers/master/_worshop.yml  
+	-e WORKSHOPS_URLS=https://raw.githubusercontent.com/gonzaloacosta/workshop-containers/master/_worshop.yml  \
+	-e JAVA_APP=false
 oc expose svc/workshop-containers
