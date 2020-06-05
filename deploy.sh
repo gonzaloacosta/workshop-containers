86202142446cb32dcd8cf769b1e660876918188e
Gonzalo Acosta <gonzaloacostapeiro@gmail.com>
2020-06-05 13:06:17 -0300

    agrego y corrijo links

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #oc new-project workshops
 oc new-app osevg/workshopper:latest --name=workshop-containers \
-	    -e WORKSHOPS_URLS=https://raw.githubusercontent.com/gonzaloacosta/workshop-containers/master/_modules.yml
+	    -e WORKSHOPS_URLS=https://raw.githubusercontent.com/gonzaloacosta/workshop-containers/master/_workshop.yml
 oc expose svc/workshop-containers
