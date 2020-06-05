1a8c27730abfe8cbb5589569c1f77ae221b74d59
Gonzalo Acosta <gonzaloacostapeiro@gmail.com>
2020-06-05 16:40:48 -0300

    modificamo el formato a adoc

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 oc new-app osevg/workshopper:latest --name=workshop-containers \
       -e WORKSHOPS_URLS="https://raw.githubusercontent.com/gonzaloacosta/workshop-containers/master/_workshop.yml" \
-      -e JAVA_APP=true
+      -e JAVA_APP=false
 oc expose svc/workshop-containers
